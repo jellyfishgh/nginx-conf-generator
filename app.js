915ba13ff3a9ca6b50df706a4aeb9087522b1f9c
hemu <hemu@cmschina.com.cn>
2017-12-19 15:30:57 +0800

    nginx conf 支持 service-worker

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 const fs = require('fs')
 
-const project = process.argv[2]
+const root = process.argv[2]
+const project = process.argv[3]
 
-const template = `location /space_holder/index.html {
-  root  /work/html;
+const template = `location /space_holder/(index.html|favicon.ico|manifest.json|service-worker.js|asset-manifest.json) {
+  root  root_holder;
   index index.html;
   break;
 }
 location ^~ /space_holder/static/ {
-  root  /work/html;
+  root  root_holder;
   index index.html;
   break;
 }
 location ^~ /space_holder/ {
-  root  /work/html;
-  rewrite ^ /space_holder/index.html  break;
-}`
+  root    root_holder;
+  rewrite ^ /space_holder/index.html break;
+}
+`
 
-if (project) {
+if (root && project) {
   console.log(`准备给 ${project} 生成 SPA Nginx conf`)
-  const conf = template.replace(/space_holder/g, project)
+  const conf = template.replace(/space_holder/g, project).replace(/root_holder/g, root)
   fs.writeFile(`${project}.conf`, conf, err => {
     if (err) throw err
     console.log(`${project}.conf 创建完成`)
   })
 } else {
   console.warn('请输入项目名称')
-  console.log('eg: node app.js sys-master')
+  console.log('eg: node app.js /work/html sys-master')
 }
